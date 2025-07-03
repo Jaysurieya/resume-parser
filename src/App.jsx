46afd85e3b5ec39cd368ef77c0a240cc71d968b1
Jaysurieya <jaysurieyayuvaraja@gmail.com>
2025-07-03 22:07:29 +0530

    DashBoard creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import { SparklesPreview } from "./components/SparklesPreview";
+import GooeyNav from "./components/GooeyNav/GooeyNav";
+import "./App.css";
 
 function App() {
-  return <SparklesPreview />;
+    const navItems = [
+        { label: "Home", href: "#home" },
+        { label: "About", href: "#about" },
+        { label: "Contact", href: "#contact" },
+      ];
+  return (
+    <div >
+      <div style={{paddingTop: "20px"}}>
+      <GooeyNav items={navItems}/> 
+      </div>
+      <SparklesPreview />
+    </div>
+  );
 }
 
 export default App;
